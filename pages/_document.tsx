a368e1bbdcc293f051687719373f964f5a197f3e
vastlaan <vastlaan@mail.com>
2021-05-03 15:56:14 +0200

    Update styles rendering in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,33 @@ import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
 
+    static async getInitialProps(ctx) {
+        // Step 1: Create an instance of ServerStyleSheet
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+
     render() {
         return (
             <Html lang="nl">
@@ -16,22 +43,6 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Oswald:wght@200;300;400;500;600;700&display=swap"
                         rel="stylesheet"
                     />
-                    {/* <script
-                        async
-                        src="https://www.googletagmanager.com/gtag/js?id=UA-132849357-5"
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `                              
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'UA-132849357-5', {
-                                    page_path: window.location.pathname,
-                                });
-                            `,
-                        }}
-                    /> */}
                 </Head>
                 <body>
                     <Main />
@@ -43,29 +54,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
-export const getServerSideProps = async (ctx : DocumentContext) => {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-        ctx.renderPage = () =>
-            originalRenderPage({
-                enhanceApp: (App) => (props) =>
-                    sheet.collectStyles(<App {...props} />),
-            });
-
-        const initialProps = await Document.getInitialProps(ctx);
-        return {
-            ...initialProps,
-            styles: (
-                <>
-                    {initialProps.styles}
-                    {sheet.getStyleElement()}
-                </>
-            ),
-        };
-    } finally {
-        sheet.seal();
-    }
-}
