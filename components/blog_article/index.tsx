5bee1393bfd63cf24e4c148dd563d218491acb8f
michal.antczak <michal.antczak@and.digital>
2023-03-25 17:31:14 +0100

    add markup

--- components/blog_article/index.tsx
+++ components/blog_article/index.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 import { Post } from "../../types";
-import { Text, SectionNarrow } from "../../styles";
+import rehypeHighlight from "rehype-highlight";
+import hljs from "highlight.js";
+import remarkGfm from "remark-gfm";
+import style from "./markdown-styles.module.css";
+import "highlight.js/styles/night-owl.css";
+import { Text, Section, ContainerNarrow, Heading1 } from "../../styles";
+import styled from "styled-components";
 
 export default function BlogArticle({ post }: { post: Post }) {
+  console.log({ post });
+
   return (
-    <SectionNarrow margin="8.7rem auto">
-      <Text>{post.content}</Text>
-    </SectionNarrow>
+    <CustomSection>
+      <ContainerNarrow>
+        <Heading1 wide="100%">{post.title}</Heading1>
+        <Text>Read time: {post.time}</Text>
+        <ReactMarkdown
+          className={style.markdown}
+          children={post.content}
+          rehypePlugins={[rehypeHighlight]}
+          remarkPlugins={[remarkGfm]}
+        />
+      </ContainerNarrow>
+    </CustomSection>
   );
 }
+
+const CustomSection = styled(Section)`
+  background-color: white;
+  margin: 8.7rem auto;
+  font-size: 1.6rem;
+  padding: 2.7rem;
+
+  h1 {
+    color: ${(p) => p.theme.greyPrimary};
+  }
+`;
