ae2109ae9ad2b506199fcfd6a5de004d2243fc82
michal.antczak <michal.antczak@and.digital>
2023-03-25 17:50:22 +0100

    fix styles for small devices

--- components/blog_article/index.tsx
+++ components/blog_article/index.tsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactMarkdown from "react-markdown";
 import { Post } from "../../types";
 import rehypeHighlight from "rehype-highlight";
-import hljs from "highlight.js";
 import remarkGfm from "remark-gfm";
-import style from "./markdown-styles.module.css";
 import "highlight.js/styles/night-owl.css";
-import { Text, Section, ContainerNarrow, Heading1 } from "../../styles";
+import {
+  Text,
+  Section,
+  ContainerNarrow,
+  Heading1,
+  respond,
+} from "../../styles";
 import styled from "styled-components";
 
 export default function BlogArticle({ post }: { post: Post }) {
-  console.log({ post });
-
   return (
     <CustomSection>
       <ContainerNarrow>
         <Heading1 wide="100%">{post.title}</Heading1>
         <Text>Read time: {post.time}</Text>
         <ReactMarkdown
-          className={style.markdown}
           children={post.content}
           rehypePlugins={[rehypeHighlight]}
           remarkPlugins={[remarkGfm]}
@@ -32,9 +33,23 @@ const CustomSection = styled(Section)`
   background-color: white;
   margin: 8.7rem auto;
   font-size: 1.6rem;
-  padding: 2.7rem;
+  padding: 2.7rem 0rem;
+
+  ${respond(
+    "s",
+    `
+      padding: 2.7rem;
+    `
+  )}
 
   h1 {
     color: ${(p) => p.theme.greyPrimary};
+  }
+  ol {
+    padding: 1.6rem;
+  }
+
+  p {
+    margin: 1.6rem 0;
   }
 `;
